06ce3783ffd67bf6c2292f93a68b18b838b1838b
chu byoungwoo <cbwstar@naver.com>
2021-02-07 15:19:05 +0900

    firebase 저장

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    <v-app-bar-nav-icon @click="drawer =!drawer"/>
+    <site-title :title="title"></site-title>
+     <v-spacer></v-spacer>
+     <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
+     <v-btn icon @click="read">read</v-btn>
+     <v-btn icon @click="readOne">readOne</v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer">
+      <site-menu></site-menu>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+    <site-footer :footer="footer"></site-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import siteTitle from '@/views/site/title'
+import siteFooter from '@/views/site/footer'
+import siteMenu from '@/views/site/menu'
 
-#nav {
-  padding: 30px;
+export default {
+  components: { siteTitle, siteFooter, siteMenu },
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      title: '나의 타이틀입니다.',
+      footer: '푸터입니다.'
+    }
+  },
+  mounted () {
+    console.log('firebase:', this.$firebase)
+  },
+  methods: {
+    save () {
+      console.log('save')
+      this.$firebase.database().ref().child('abcd').set({
+        title: 'abcd',
+        text: 'tttt'
+      })
+    },
+    read () {
+      this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+        console.log(sn)
+        console.log(sn.val())
+      })
+    },
+    async readOne () {
+      const sn = await this.$firebase.database().ref().child('abcd').once('value')
+      console.log('readOne : ', sn.val())
+    }
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
