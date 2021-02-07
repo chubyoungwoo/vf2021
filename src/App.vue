bc40ba814f54ed546ecd47a25aca903c7d25548b
chu byoungwoo <cbwstar@naver.com>
2021-02-07 18:57:34 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,18 @@
       dark
     >
     <v-app-bar-nav-icon @click="drawer =!drawer"/>
-    <site-title :title="title"></site-title>
+    <site-title :title="site.title"></site-title>
      <v-spacer></v-spacer>
-     <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
-     <v-btn icon @click="read">read</v-btn>
-     <v-btn icon @click="readOne">readOne</v-btn>
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer">
-      <site-menu></site-menu>
+      <site-menu :items="site.menu"></site-menu>
     </v-navigation-drawer>
 
     <v-content>
       <router-view/>
     </v-content>
-    <site-footer :footer="footer"></site-footer>
+    <site-footer :footer="site.footer"></site-footer>
   </v-app>
 </template>
 
@@ -35,29 +32,72 @@ export default {
   data () {
     return {
       drawer: false,
-      title: '나의 타이틀입니다.',
-      footer: '푸터입니다.'
+      site: {
+        menu: [
+          {
+            title: 'home',
+            icon: 'mdi-home',
+            subItems: [
+              {
+                title: 'Dashboard',
+                to: '/'
+              },
+              {
+                title: 'About',
+                to: '/about'
+              }
+
+            ]
+          },
+          {
+            title: 'about',
+            active: true,
+            icon: 'mdi-account',
+            subItems: [
+              {
+                title: 'xxx',
+                to: '/xxx'
+              }
+            ]
+          }
+        ],
+        title: '나의 타이틀입니다.',
+        footer: '푸터입니다.'
+      }
     }
   },
-  mounted () {
-    console.log('firebase:', this.$firebase)
+  created () {
+    this.subscribe()
   },
   methods: {
+    subscribe () {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+          return
+        }
+        this.site = v
+        console.log('this.site :', this.site)
+      }, (e) => {
+        console.log(e.message)
+      })
+    },
     save () {
       console.log('save')
-      this.$firebase.database().ref().child('abcd').set({
+      this.$firebase.database().ref().child('site').set({
         title: 'abcd',
         text: 'tttt'
       })
     },
     read () {
-      this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
         console.log(sn)
-        console.log(sn.val())
+        console.log('site :', sn.val())
       })
     },
     async readOne () {
-      const sn = await this.$firebase.database().ref().child('abcd').once('value')
+      const sn = await this.$firebase.database().ref().child('site').once('value')
       console.log('readOne : ', sn.val())
     }
   }
